5cbe2abaaa24c9b48a2c8d430a63113af4169ff1
soramame0256 <sora200507@gmail.com>
2022-08-08 12:03:28 +0900

    あぷでだーーー

--- src/main/java/com/github/soramame0256/pokemobdetail/PokeMobDetail.java
+++ src/main/java/com/github/soramame0256/pokemobdetail/PokeMobDetail.java
@@ -20,7 +20,7 @@ public class PokeMobDetail {
 
     public static final String MOD_ID = "pokemobdetail";
     public static final String MOD_NAME = "PokeMobDetail";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0.1";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
